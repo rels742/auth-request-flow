11f2e0e6837d7ab382887f9a4257d5ebc158a9d7
Lorel <lorel.oboue@gmail.com>
2022-09-07 23:46:45 +0100

    post login update

--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
+  const token = jwt.sign({ username: mockUser.username }, "secretsafewithme");
 
+  //response in json object
+  res.json({ token });
 });
 
-router.get('/profile', (req, res) => {
-  
-});
-
+router.get("/profile", (req, res) => {});
 
 module.exports = router;
